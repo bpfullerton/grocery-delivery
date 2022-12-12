d9b3dfe7a7d0fda79db3f70baa911d0156e82fb0
Alex Miale <alexmiale2014@gmail.com>
2022-12-12 18:15:43 -0500

    update cart

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -5,8 +5,8 @@ from src import db
 
 customers = Blueprint('customers', __name__)
 
-# Get all customers from the DB
-@customers.route('/customers', methods=['GET'])
+# TODO: implement adding to cart
+@customers.route('/addCart', methods=['POST'])
 def get_customers():
     cursor = db.get_db().cursor()
     cursor.execute('select customerNumber, customerName,\
@@ -23,9 +23,9 @@ def get_customers():
 
 # Get customer cart details
 @customers.route('/cart', methods=['GET'])
-def get_customer(customerId):
+def get_customer(customerId, ):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where customerNumber = {0}'.format(customerId))
+    cursor.execute('select itemName from OrderItem where orderID = (select orderID From Order where OrderedBy = {0})'.format(customerId))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -37,8 +37,9 @@ def get_customer(customerId):
     return the_response
 
     
+# add credit card
 @customers.route('/addCreditCard', methods = ['POST'])
-def add_item(customerId):
+def add_cc(customerId):
     current_app.logger.info(request.form)
     cursor = db.get_db().cursor()
     number = request.form['cardNumber']
@@ -50,5 +51,19 @@ def add_item(customerId):
     query2 = f'update Customer set creditCard = {0} where customerID = {1}'.format(number, customerId)
     cursor.execute(query)
     cursor.execute(query2)
+    db.get_db().commit()
+    return "Success!"
+
+# add other payment method
+@customers.route('/addPayment', methods = ['POST'])
+def add_payment(customerId):
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    bankAccount = request.form['bankAccount']
+    url = request.form['url']
+    query = 'update Customer set bankAcctNum = {0} where customerID = {1}'.format(bankAccount, customerId)
+    query2 = 'update Customer set paymentApp = {0} where customerID = {1}'.format(url, customerId)
+    cursor.execute(query)
+    cursor.execut(query2)
     db.get_db().commit()
     return "Success!"
