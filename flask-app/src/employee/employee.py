a029ac79ba101427bea3cd30633ba644346fbd6a
Alex Miale <alexmiale2014@gmail.com>
2022-12-13 12:56:22 -0500

    employee orders

--- flask-app/src/employee/employee.py
+++ flask-app/src/employee/employee.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -6,42 +6,13 @@ from src import db
 employee = Blueprint('employee', __name__)
 
 # Get all the products from the database
-@employee.route('/products', methods=['GET'])
-def get_products():
+@employee.route('/id', methods=['POST'])
+def get_id():
     # get a cursor object from the database
+    current_app.logger.info(request.form)
     cursor = db.get_db().cursor()
-
-    # use cursor to query the database for a list of products
-    cursor.execute('select productCode, productName, productVendor from products')
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
-# get the top 5 products from the database
-@employee.route('/top5products')
-def get_most_pop_products():
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT p.productCode, productName, sum(quantityOrdered) as totalOrders
-        FROM products p JOIN orderdetails od on p.productCode = od.productCode
-        GROUP BY p.productCode, productName
-        ORDER BY totalOrders DESC
-        LIMIT 5;
-    '''
+    id = request.form['id']
+    query = f'SELECT * FROM Orders WHERE fulfilledBy = \"{id}\"'
     cursor.execute(query)
        # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -52,10 +23,9 @@ def get_most_pop_products():
 
     # fetch all the data from the cursor
     theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
